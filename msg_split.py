e7c68792ad7601933cbb01058d79fab0c1c2e3a3
Stepan <stepan.bakshaev@keemail.me>
2024-12-15 17:01:54 +0200

    explore idea of list of strings and mutable environment

--- msg_split.py
+++ msg_split.py
@@ -7,13 +7,15 @@ Author Stepan Bakshaev, 2024.
 Contact stepan.bakshaev@keemail.me
 """
 from argparse import ArgumentParser
+from copy import deepcopy
+from dataclasses import dataclass
 from enum import Enum
 from itertools import chain
 from operator import attrgetter, itemgetter
 from typing import Iterator, Type
 
 from bs4 import BeautifulSoup
-from bs4.element import Tag, PageElement
+from bs4.element import Tag, PageElement, NavigableString
 
 MAX_LEN = 4096
 
@@ -22,7 +24,7 @@ class UnprocessedValue(Exception):
     pass
 
 
-split_tags = frozenset("p b strong i ul ol div span".split(' '))  # ? | {BeautifulSoup.ROOT_TAG_NAME}
+split_tags = frozenset("p b strong i ul ol div span".split(' ')) | {BeautifulSoup.ROOT_TAG_NAME}
 
 
 def measure(node: Type[PageElement], eventual_encoding, formatter) -> int:
@@ -38,22 +40,95 @@ def measure(node: Type[PageElement], eventual_encoding, formatter) -> int:
         )
         return len(piece) + len(piece_end)
     else:
-        piece = node.output_ready(formatter=None)
+        piece = node.output_ready(formatter=formatter)
         return len(piece)
 
 
-def walk(source_node: Type[Tag], fragment_tree: PageElement, consumed: int, max_len: int, eventual_encoding, formatter) -> Iterator[Tag]:
+@dataclass
+class Environment:
+    """
+    yield grabs return value. Use mutable object as data exchange in between recursive calls.
+    """
+    consumed: int  # overall length of fragment
+    max_len: int  # limit for length
+    forward: list[str]  # leading text chunks of fragment
+    # forward_prefix_sum: list[int]
+    backward: list[str] # closing tags, tail of text chunks of fragment
+    # backward_prefix_sum: list[int]
+    first_child: list[bool]  # it is used for cut parent-first_child from fragment.
+    eventual_encoding: str  # BeautifulSoup
+    formatter: str  # BeautifulSoup
+
+
+def walk(source_node: PageElement, environment: Environment, first_child) -> Iterator[Tag]:
     """
     Simple and recursive to be understandable by junior developer... by all time complexity consts.
-
-    Fill fragment_tree from source_node until max_length. Yield each time max_length is exceeded.
     """
     # Measure current budget of the node.
-    budget = measure(source_node, eventual_encoding, formatter)
+    # The trick is in a case of atomic node take all content. Hence, nothing to inspect on deeper level.
+    forward = ''
+    backward = ''
+    # contents makes walk go deeper or not.
+    contents = ()
+    if isinstance(source_node, Tag):
+        if source_node.name not in split_tags:
+            forward = source_node.decode(formatter=environment.formatter)
+        else:
+            contents = source_node.contents
+            forward = source_node._format_tag(
+                environment.eventual_encoding, environment.formatter, opening=True
+            )
+            if not source_node.is_empty_element:
+                backward = source_node._format_tag(
+                    environment.eventual_encoding, environment.formatter, opening=False
+                )
+    else:
+        assert isinstance(source_node, NavigableString), f'Unhandled type {type(source_node)}.'
+        forward = source_node.output_ready(formatter=environment.formatter)
+
+    length = len(forward) + len(backward)
 
     # Make sure there is a space to put the node. Otherwise, drain.
-    if budget + consumed > max_len:
-        # prepare fragment_tree to output.
+    if length + environment.consumed > environment.max_len:
+        # First, calculate new consumption to figure out fitting into limit.
+        # backward stays the same.
+        consumed = sum(map(len, environment.backward))
+        # forward is rebuilt by source_node parents.
+        fresh_forward = []
+        for node in source_node.parents:
+            opening = node._format_tag(
+                environment.eventual_encoding, environment.formatter, opening=True
+            )
+            consumed += len(opening)
+            fresh_forward.append(opening)
+
+        if length + consumed > environment.max_len:
+            raise UnprocessedValue(
+                f'{source_node.sourceline}:{source_node.sourcepos}: piece {forward[:38]!r}..{backward[:38]!r} and html around cannot fit max_len ({environment.max_len}).',
+                source_node.sourceline, source_node.sourcepos, forward+backward, environment.max_len
+            )
+
+        forward_severed = len(environment.forward)
+        backward_severed = len(environment.backward)
+        # remove parent-first_child chain from fragment.
+        if first_child:
+            forward_severed -= 1
+            backward_severed -= 1
+            for marker in reversed(environment.first_child):
+                if not marker:
+                    break
+                forward_severed -= 1
+                backward_severed -= 1
+
+        # make fragment
+        yield ''.join(chain(environment.forward[:forward_severed], reversed(environment.backward[:backward_severed])))
+
+        # fill forward and backward with source_node parents.
+
+
+
+
+        if source_node.parent is not
         yield fragment_tree
 
         # build new tree.
